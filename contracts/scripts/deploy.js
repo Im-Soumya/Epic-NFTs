1263d26f85b87642367768312f6222b4bfe11686
Soumya <aeie2019@rcciit.org.in>
2022-07-05 16:00:40 +0530

    Added react app

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -2,18 +2,12 @@ const main = async () => {
   const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
-  console.log("Contract deployed to:", nftContract.address);
+  console.log("Contract deployed: ", nftContract.address);
 
-  // Call the function.
-  let txn = await nftContract.makeAnEpicNFT()
-  // Wait for it to be mined.
-  await txn.wait()
-  console.log("Minted NFT #1")
+  let txn = await nftContract.makeAnEpicNFT();
+  await txn.wait();
 
-  txn = await nftContract.makeAnEpicNFT()
-  // Wait for it to be mined.
-  await txn.wait()
-  console.log("Minted NFT #2")
+  console.log("Minted NFT #1");
 };
 
 const runMain = async () => {
--- contracts/scripts/run.js
+++ contracts/scripts/run.js
@@ -1,52 +1,22 @@
-// const main = async () => {
-//     const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
-//     const nftContract = await nftContractFactory.deploy();
-//     await nftContract.deployed();
-
-//     console.log("Contract address: ", nftContract.address);
-
-//     let txn = await nftContract.makeAnEpicNFT();
-//     await txn.wait();
-// }
-
-// const runMain = async () => {
-//     try {
-//         await main();
-//         process.exit(0);
-//     } catch(e) {
-//         console.log(e);
-//         process.exit(1);
-//     }
-// }
-
-// runMain();
-
 const main = async () => {
     const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
-    console.log("Contract deployed to:", nftContract.address);
-  
-    // Call the function.
-    let txn = await nftContract.makeAnEpicNFT()
-    // Wait for it to be mined.
-    await txn.wait()
-  
-    // Mint another NFT for fun.
-    txn = await nftContract.makeAnEpicNFT()
-    // Wait for it to be mined.
-    await txn.wait()
-  
-  };
-  
-  const runMain = async () => {
+
+    console.log("Contract address: ", nftContract.address);
+
+    let txn = await nftContract.makeAnEpicNFT();
+    await txn.wait();
+}
+
+const runMain = async () => {
     try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
+        await main();
+        process.exit(0);
+    } catch(e) {
+        console.log(e);
+        process.exit(1);
     }
-  };
-  
-  runMain();
+}
+
+runMain();
