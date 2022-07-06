12958cfe28112762e0aa499a0720c1e1bfbdef11
Soumya <aeie2019@rcciit.org.in>
2022-07-06 20:58:35 +0530

    Added minting button

--- contracts/contracts/MyEpicNFT.sol
+++ contracts/contracts/MyEpicNFT.sol
@@ -9,10 +9,11 @@ import "hardhat/console.sol";
 
 import {Base64} from "./libraries/Base64.sol";
 
-contract MyEpicNFT is ERC721URIStorage{
-
+contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+
+    uint256 maxNFTs = 50;
 
     string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
     string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
@@ -22,30 +23,32 @@ contract MyEpicNFT is ERC721URIStorage{
     string[] thirdWords = ["Cupcake", "Pizza", "Milkshake", "Curry", "Chicken", "Salad", "Sandwich"];
     string[] colours = ["#183182", "#34073d", "#ef745c", "#007f5f", "#333533", "#720026"];
 
+    event NewNFTMinted(address sender, uint256 tokenId);
+
     constructor() ERC721("Square NFTs", "SQR") {
         console.log("This is a NFT smart contract");
     }
 
     function findFirstWord(uint256 tokenId) public view returns (string memory) {
-        uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
-        rand = rand  % firstWords.length;
+        uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));   
+        rand = rand % firstWords.length;
         return firstWords[rand];
     }
 
-    function findSecondWord(uint256 tokenId) public view returns (string memory) {
+    function findSecondWord(uint256 tokenId) public view returns(string memory) {
         uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
         rand = rand % secondWords.length;
         return secondWords[rand];
     }
 
-    function findThirdWord (uint256 tokenId) public view returns (string memory) {
+    function findThirdWord(uint256 tokenId) public view returns(string memory) {
         uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
         rand = rand % thirdWords.length;
         return thirdWords[rand];
     }
 
-    function findRandomColor (uint256 tokenId) public view returns (string memory) {
-        uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
+    function findRandomColour(uint256 tokenId) public view returns(string memory) {
+        uint256 rand = random(string(abi.encodePacked("COLOURS", Strings.toString(tokenId))));
         rand = rand % colours.length;
         return colours[rand];
     }
@@ -54,22 +57,28 @@ contract MyEpicNFT is ERC721URIStorage{
         return uint256(keccak256(abi.encodePacked(input)));
     }
 
+    function getNFTsMinted() public view returns(uint256) {
+        return _tokenIds.current();
+    }
+
     function makeAnEpicNFT() public {
         uint256 newItemId = _tokenIds.current();
 
-        string memory firstWord = findFirstWord(newItemId); 
-        string memory secondWord = findSecondWord(newItemId); 
-        string memory thirdWord = findThirdWord(newItemId); 
-        string memory colour = findRandomColor(newItemId);
+        require(newItemId < maxNFTs, "NFTs limit exceeded");
 
-        string memory combinedword = string(abi.encodePacked(firstWord, secondWord, thirdWord));
-        string memory finalSvg = string(abi.encodePacked(svgPartOne, colour, svgPartTwo, combinedword, "</text></svg>"));
+        string memory firstWord = findFirstWord(newItemId);
+        string memory secondWord = findSecondWord(newItemId);
+        string memory thirdWord = findThirdWord(newItemId);
+        string memory colour = findRandomColour(newItemId);
+
+        string memory combinedWord = string(abi.encodePacked(firstWord, secondWord, thirdWord));
+        string memory finalSvg = string(abi.encodePacked(svgPartOne, colour, svgPartTwo, combinedWord, "</text></svg>"));
 
         string memory json = Base64.encode(
             bytes(
                 string(
                     abi.encodePacked(
-                        '{"name": "',combinedword,'", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',Base64.encode(bytes(finalSvg)),'"}'
+                        '{"name": "', combinedWord, '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',Base64.encode(bytes(finalSvg)), '"}'
                     )
                 )
             )
@@ -77,14 +86,16 @@ contract MyEpicNFT is ERC721URIStorage{
 
         string memory finalURI = string(abi.encodePacked("data:application/json;base64,", json));
 
-        console.log("\n---------------");
+        console.log("\n--------------------");
         console.log(finalURI);
-        console.log("---------------\n");
+        console.log("\n--------------------");
 
         _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, "finalURI");
-        
-        _tokenIds.increment();
+        _setTokenURI(newItemId, finalURI);
+
         console.log("An NFT with ID %s has been minted to %s", newItemId, msg.sender);
+        _tokenIds.increment();
+
+        emit NewNFTMinted(msg.sender, newItemId);
     }
 }
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,22 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState } from "react";
 import { ethers } from "ethers";
 import contractAbi from "./utils/MyEpicNFT.json";
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
-const TOTAL_MINT_COUNT = 50;
+const OPENSEA_LINK = 'https://testnets.opensea.io/collection/square-nfts-8tc1n7h1rz';
 
 function App() {
 
   const [currentAccount, setCurrentAccount] = useState("");
+  const [nftsMinted, setNftsMinted] = useState(0);
+  const [minting, setMinting] = useState(false);
 
-  const contractAddress = "0x400F0f00FAD5d29b8Df3f760fd302CED925F56db";
+  const contractAddress = "0xAC5B3B3Dad1b8F7C3253cc73Fa2A56913E9B9EFF";
+  const rinkebyChainId = "0x4";
 
   const checkWalletConnected = async () => {
     const { ethereum } = window;
@@ -23,15 +25,24 @@ function App() {
     } else {
       console.log("Get Metamask");
     }
+    
+    let chainId = await ethereum.request({method: "eth_chainId"});
+    console.log("Connected to chain " + chainId);
+
+    if(chainId !== rinkebyChainId) {
+      alert("Change your network to Rinkeby");
+    }    
 
     const accounts = await ethereum.request({ method: "eth_accounts" });
     if (accounts.length !== 0) {
       console.log("Authorised account detected", accounts[0]);
-      setCurrentAccount(accounts[0]);
-    } else {
-      console.log("No authosrised account found");
-    }
-  }
+      setCurrentAccount(accounts[0]);    
+      getTotalNFTsMinted();
+      setupEventListener();
+      } else {
+        console.log("No authosrised account found");
+      }
+  };
 
   const connectWallet = async () => {
     try {
@@ -42,12 +53,9 @@ function App() {
       }
 
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-      if (accounts !== 0) {
-        console.log("Connected", accounts[0]);
-        setCurrentAccount(accounts[0]);
-      } else {
-        console.log("No authorised account found");
-      }
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+      // setupEventListener();
     } catch (e) {
       console.error(e);
     }
@@ -62,18 +70,58 @@ function App() {
         const nftContract = new ethers.Contract(contractAddress, contractAbi.abi, signer);
 
         let nftTxn = await nftContract.makeAnEpicNFT();
+        setMinting(true);
         console.log("Mining...");
 
         await nftTxn.wait();
-        console.log("Mined...");
-
+        setMinting(false);
+        getTotalNFTsMinted();
         console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
-      } else {
-        alert("Get Metamask");
-        return;
-      }
+      } 
     } catch (e) {
       console.error(e);
+    }
+  }
+
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, contractAbi.abi, signer);
+
+        console.log(parseInt(ethereum.chainId, 16));
+        
+        nftContract.on("NewNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${contractAddress}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getTotalNFTsMinted = async () => {
+    try {
+      const {ethereum} = window;
+      if(ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, contractAbi.abi, signer);
+
+        let mintedNFTs = await nftContract.getNFTsMinted();
+        setNftsMinted(mintedNFTs);
+      }
+    } catch(e) {
+      console.log(e);
     }
   }
 
@@ -87,11 +135,13 @@ function App() {
     )
   }
 
-  const renderMintButton = () => {
+  const renderMintNFTButton = () => {
     return (
       <div>
         <button onClick={mintNFt} className='cta-button connect-wallet-button'>
-          Mint NFT
+          {
+            minting ? "Minting..." : "Mint NFT"
+          }
         </button>
       </div>
     )
@@ -114,18 +164,40 @@ function App() {
               renderConnectWalletButton()
             ) :
             (
-              renderMintButton()
+              renderMintNFTButton()
+            )
+          }
+          {
+            nftsMinted > 0 ?
+            (
+              <p style={{color: "white"}}>{`${nftsMinted}/50 NFTs minted till now.`}</p>
+              ) : 
+              (
+              <p style={{color: "white"}}>{`0/50 NFTs minted till now.`}</p>
             )
           }
         </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+        <div>
+          <div className="footer-container">
+            <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+            <a
+              className="footer-text"
+              href={TWITTER_LINK}
+              target="_blank"
+              rel="noreferrer"
+            >{`built on @${TWITTER_HANDLE}`}</a>
+          </div>
+          <div className="footer-container">
+            <img alt=""/>
+            <a
+              className='footer-text'
+              href={OPENSEA_LINK}
+              target="_blank"
+              rel="noreferrer"
+            >
+            🌊 View Collection on OpenSea
+            </a>
+          </div>
         </div>
       </div>
     </div>
